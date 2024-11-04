36032038046596f102552304cacce3596e9830d9
Khaled Abuhaltam <khaled.abuhaltam@mni.thm.de>
2024-11-04 11:05:31 +0100

    Initial Commit

---
+++ Finanztracker/FinanztrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanztrackerApp.swift
+//  Finanztracker
+//
+//  Created by Khaled Abuhaltam on 04.11.24.
+//
+
+import SwiftUI
+
+@main
+struct FinanztrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinanztrackerTests/FinanztrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinanztrackerTests.swift
+//  FinanztrackerTests
+//
+//  Created by Khaled Abuhaltam on 04.11.24.
+//
+
+import Testing
+@testable import Finanztracker
+
+struct FinanztrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
